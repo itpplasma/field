91439d437bf983ca1996a31a899b0a5ce4ad1dc1
GeorgGrassler <georg.grassler@tugraz.at>
2024-08-09 14:09:40 +0200

    added curla_test biotsavart_field
    * shifted field calc utils for testing in seperate modul
    * minor cleanup of example_field tests
    * added test coil file tools for biotsavart_field tests

--- test/test_libneo_biotsavart_field.f90
+++ test/test_libneo_biotsavart_field.f90
@@ -43,25 +43,24 @@ end subroutine test_biotsavart_field_init
 
 subroutine test_curla_equal_b
     use libneo_biotsavart_field, only: biotsavart_field_t
+    use test_libneo_field_util, only: compute_cartesian_curla
 
     real(dp), parameter :: tol = 1.0e-9_dp
 
+    character(*), parameter :: coils_file = "test_coils.txt"
     type(biotsavart_field_t) :: biotsavart_field
-    real(dp) :: x(3), A(3), B(3), B_from_A(3)
-    real(dp) :: temp(3)
+    real(dp) :: x(3), B(3), B_from_A(3)
 
     call print_test("test_curla_equal_b")
 
-    print*, "Not implemented"
-    call print_fail
-    return
-
-    call biotsavart_field%biotsavart_field_init()
+    call create_straight_wire_coils_file(coils_file)
+    call biotsavart_field%biotsavart_field_init(coils_file)
+    call remove_test_coils_file(coils_file)
 
     x = [1.0_dp, 1.0_dp, 1.0_dp]
 
     call biotsavart_field%compute_bfield(x, B)
-    B_from_A = compute_curla(biotsavart_field, x, tol)
+    B_from_A = compute_cartesian_curla(biotsavart_field, x, tol)
 
     if (maxval(abs(B - B_from_A)) > tol) then
         print *, "curl A != B"
@@ -69,52 +68,44 @@ subroutine test_curla_equal_b
         return
     end if
 
-    call biotsavart_field%compute_abfield(x, A, B)
-    call biotsavart_field%compute_afield(x, temp)
-    if (maxval(abs(A - temp)) > tol) then
-        print *, "A from afield != from abfield"
-        call print_fail
-        return
-    end if
-    call biotsavart_field%compute_bfield(x, temp)
-    if (maxval(abs(B - temp)) > tol) then
-        print *, "B from bfield != from abfield"
-        call print_fail
-        return
-    end if
-
     call print_ok
 end subroutine test_curla_equal_b
 
-
-function compute_curla(field, x, tol) result(curla)
-    use libneo_field_base, only: field_t
-
-    class(field_t), intent(in) :: field
-    real(dp), intent(in) :: x(3)
-    real(dp), intent(in) :: tol
-    real(dp) :: curla(3)
-
-    real(dp) :: x_temp(3), A_temp1(3), A_temp2(3)
-    real(dp) :: dA_dx(3,3), delta
-    integer :: i
-
-    delta = sqrt(tol)
-    x_temp = x
-    dA_dx = 0.0_dp
-    do i = 1, 3
-        x_temp = x
-        x_temp(i) = x(i) + delta
-        call field%compute_afield(x_temp, A_temp1)
-        x_temp(i) = x(i) - delta
-        call field%compute_afield(x_temp, A_temp2)
-        dA_dx(i,:) = (A_temp1 - A_temp2) / (2.0_dp * delta)
-    end do
-
-    curla(1) = dA_dx(2,3) - dA_dx(3,2)
-    curla(2) = dA_dx(3,1) - dA_dx(1,3)
-    curla(3) = dA_dx(1,2) - dA_dx(2,1)
-end function compute_curla
-
     
+subroutine create_straight_wire_coils_file(filename)
+    use biotsavart, only: coils_t, save_coils_to_file
+
+    character(len=*), intent(in) :: filename
+
+    type(coils_t) :: coils
+
+    call init_straight_wire_coils(coils)
+    call save_coils_to_file(filename, coils)
+end subroutine create_straight_wire_coils_file
+
+
+subroutine init_straight_wire_coils(coils)
+    use biotsavart, only: coils_t, coils_init
+
+    type(coils_t), intent(out) :: coils
+
+    real(dp), dimension(4) :: x, y, z, current
+    real(dp), parameter    :: L = 1.0e3_dp
+
+    x = [0.0d0, 0.0d0, 0.0d0, 0.0d0]
+    y = [0.0d0, 0.0d0, 0.0d0, 0.0d0]
+    z = [-L/2.0d0, -L/2.5d0, 0.0d0, L/2.0d0]
+    current = [1.0d0, 1.0d0, 1.0d0, 0.0d0]
+
+    call coils_init(x, y, z, current, coils)
+end subroutine init_straight_wire_coils
+
+
+subroutine remove_test_coils_file(filename)
+    character(len=*), intent(in) :: filename
+
+    call system("rm -f " // filename)
+end subroutine remove_test_coils_file
+
+
 end program test_example_field
--- test/test_libneo_example_field.f90
+++ test/test_libneo_example_field.f90
@@ -40,12 +40,12 @@ end subroutine test_example_field_init
 
 subroutine test_curla_equal_b
     use libneo_example_field, only: example_field_t
+    use test_libneo_field_util, only: compute_cartesian_curla
 
     real(dp), parameter :: tol = 1.0e-9_dp
 
     type(example_field_t) :: example_field
-    real(dp) :: x(3), A(3), B(3), B_from_A(3)
-    real(dp) :: temp(3)
+    real(dp) :: x(3), B(3), B_from_A(3)
 
     call print_test("test_curla_equal_b")
 
@@ -54,24 +54,10 @@ subroutine test_curla_equal_b
     x = [1.0_dp, 1.0_dp, 1.0_dp]
 
     call example_field%compute_bfield(x, B)
-    B_from_A = compute_curla(example_field, x, tol)
+    B_from_A = compute_cartesian_curla(example_field, x, tol)
 
     if (maxval(abs(B - B_from_A)) > tol) then
         print *, "curl A != B"
-        call print_fail
-        return
-    end if
-
-    call example_field%compute_abfield(x, A, B)
-    call example_field%compute_afield(x, temp)
-    if (maxval(abs(A - temp)) > tol) then
-        print *, "A from afield != from abfield"
-        call print_fail
-        return
-    end if
-    call example_field%compute_bfield(x, temp)
-    if (maxval(abs(B - temp)) > tol) then
-        print *, "B from bfield != from abfield"
         call print_fail
         return
     end if
@@ -80,38 +66,9 @@ subroutine test_curla_equal_b
 end subroutine test_curla_equal_b
 
 
-function compute_curla(field, x, tol) result(curla)
-    use libneo_field_base, only: field_t
-
-    class(field_t), intent(in) :: field
-    real(dp), intent(in) :: x(3)
-    real(dp), intent(in) :: tol
-    real(dp) :: curla(3)
-
-    real(dp) :: x_temp(3), A_temp1(3), A_temp2(3)
-    real(dp) :: dA_dx(3,3), delta
-    integer :: i
-
-    delta = sqrt(tol)
-    x_temp = x
-    dA_dx = 0.0_dp
-    do i = 1, 3
-        x_temp = x
-        x_temp(i) = x(i) + delta
-        call field%compute_afield(x_temp, A_temp1)
-        x_temp(i) = x(i) - delta
-        call field%compute_afield(x_temp, A_temp2)
-        dA_dx(i,:) = (A_temp1 - A_temp2) / (2.0_dp * delta)
-    end do
-
-    curla(1) = dA_dx(2,3) - dA_dx(3,2)
-    curla(2) = dA_dx(3,1) - dA_dx(1,3)
-    curla(3) = dA_dx(1,2) - dA_dx(2,1)
-end function compute_curla
-
-
 subroutine test_divb_0
     use libneo_example_field, only: example_field_t
+    use test_libneo_field_util, only: compute_cartesian_divb
 
     real(dp), parameter :: tol = 1.0e-9_dp
 
@@ -124,7 +81,7 @@ subroutine test_divb_0
 
     x = [1.0_dp, 1.0_dp, 1.0_dp]
 
-    divb = compute_divb(example_field, x, tol)
+    divb = compute_cartesian_divb(example_field, x, tol)
 
     if (abs(divb) > tol) then
         print *, "div B = ", divb
@@ -134,34 +91,6 @@ subroutine test_divb_0
 
     call print_ok
 end subroutine test_divb_0
-
-
-function compute_divb(field, x, tol) result(divb)
-    use libneo_field_base, only: field_t
-
-    class(field_t), intent(in) :: field
-    real(dp), intent(in) :: x(3)
-    real(dp), intent(in) :: tol
-    real(dp) :: divb
-
-    real(dp) :: x_temp(3), B_temp1(3), B_temp2(3)
-    real(dp) :: dB_dx(3), delta
-    integer :: i
-
-    delta = sqrt(tol)
-    x_temp = x
-    dB_dx = 0.0_dp
-    do i = 1, 3
-        x_temp = x
-        x_temp(i) = x(i) + delta
-        call field%compute_bfield(x_temp, B_temp1)
-        x_temp(i) = x(i) - delta
-        call field%compute_bfield(x_temp, B_temp2)
-        dB_dx(i) = (B_temp1(i) - B_temp2(i)) / (2.0_dp * delta)
-    end do
-
-    divb = sum(dB_dx)
-end function compute_divb
 
 
 subroutine test_compute_abfield
